3fb33d6554d25b6efc32ae54cfc00de6d6bacfdc
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 16:24:02 +0530

    Calulating Singlet Combination And Percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,10 +2,25 @@
 
 echo "This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet"
 
-toss=$((RANDOM%2))
-if ((toss==1))
-then
-        echo "Head"
-else
-        echo "Tail"
-fi
+counter=10
+declare -A SingletDict
+Hcount=0
+Tcount=0
+while ((counter>0))
+do
+	toss=$((RANDOM%2))
+	if ((toss==1))
+	then
+	        ((Hcount++))
+	else
+	        ((Tcount++))
+	fi
+	((counter--))
+done
+SingletDict[H]=$Hcount
+SingletDict[T]=$Tcount
+echo ${!SingletDict[@]}
+echo ${SingletDict[@]}
+echo "H combination percentage is:" `awk "BEGIN {print $Hcount/(($Hcount+$Tcount))*100}"`
+echo "T combination percentage is:" `awk "BEGIN {print $Tcount/(($Hcount+$Tcount))*100}"`
+

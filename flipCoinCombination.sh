82f485820df6c6fae45f55eea1cef462e05de797
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 17:03:00 +0530

    Adding Triplet combination and percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -58,3 +58,65 @@ echo "HH combination percentage is:" `awk "BEGIN {print $HHcount/(($HHcount+$HTc
 echo "HT combination percentage is:" `awk "BEGIN {print $HTcount/(($HHcount+$HTcount+$THcount+$TTcount))*100}"`
 echo "TH combination percentage is:" `awk "BEGIN {print $THcount/(($HHcount+$HTcount+$THcount+$TTcount))*100}"`
 echo "TT combination percentage is:" `awk "BEGIN {print $TTcount/(($HHcount+$HTcount+$THcount+$TTcount))*100}"`
+
+counter=10
+declare -A TripletDict
+HHHcount=0
+HHTcount=0
+HTHcount=0
+HTTcount=0
+THHcount=0
+THTcount=0
+TTHcount=0
+TTTcount=0
+
+while ((counter>0))
+do
+        toss1=$((RANDOM%2))
+        toss2=$((RANDOM%2))
+	toss3=$((RANDOM%2))
+        if (( ((toss==0)) && ((toss2==0)) && ((toss3==0)) ))
+        then
+                ((HHHcount++))
+        elif (( ((toss==0)) && ((toss2==0)) && ((toss3==1)) ))
+        then
+                ((HHTcount++))
+        elif (( ((toss==0)) && ((toss2==1)) && ((toss3==0)) ))
+        then
+                ((HTHcount++))
+        elif (( ((toss==0)) && ((toss2==1)) && ((toss3==1)) ))
+	then
+                ((HTTcount++))
+	elif (( ((toss==1)) && ((toss2==0)) && ((toss3==0)) ))
+        then
+                ((THHcount++))
+        elif (( ((toss==1)) && ((toss2==0)) && ((toss3==1)) ))
+        then
+                ((THTcount++))
+        elif (( ((toss==1)) && ((toss2==1)) && ((toss3==0)) ))
+        then
+                ((TTHcount++))
+	else
+                ((TTTcount++))
+        fi
+        ((counter--))
+done
+
+TripletDict[HHH]=$HHHcount
+TripletDict[HHT]=$HHTcount
+TripletDict[HTH]=$HTHcount
+TripletDict[HTT]=$HTTcount
+TripletDict[THH]=$THHcount
+TripletDict[THT]=$THTcount
+TripletDict[TTH]=$TTHcount
+TripletDict[TTT]=$TTTcount
+echo ${!TripletDict[@]}
+echo ${TripletDict[@]}
+echo "HHH combination percentage is:" `awk "BEGIN {print $HHHcount/(($HHHcount+$HHTcount+$HTHcount+$HTTcount+$THHcount+$THTcount+$TTHcount+$TTTcount))*100}"`
+echo "HHT combination percentage is:" `awk "BEGIN {print $HHTcount/(($HHHcount+$HHTcount+$HTHcount+$HTTcount+$THHcount+$THTcount+$TTHcount+$TTTcount))*100}"`
+echo "HTH combination percentage is:" `awk "BEGIN {print $HTHcount/(($HHHcount+$HHTcount+$HTHcount+$HTTcount+$THHcount+$THTcount+$TTHcount+$TTTcount))*100}"`
+echo "HTT combination percentage is:" `awk "BEGIN {print $HTTcount/(($HHHcount+$HHTcount+$HTHcount+$HTTcount+$THHcount+$THTcount+$TTHcount+$TTTcount))*100}"`
+echo "THH combination percentage is:" `awk "BEGIN {print $THHcount/(($HHHcount+$HHTcount+$HTHcount+$HTTcount+$THHcount+$THTcount+$TTHcount+$TTTcount))*100}"`
+echo "THT combination percentage is:" `awk "BEGIN {print $THTcount/(($HHHcount+$HHTcount+$HTHcount+$HTTcount+$THHcount+$THTcount+$TTHcount+$TTTcount))*100}"`
+echo "TTH combination percentage is:" `awk "BEGIN {print $TTHcount/(($HHHcount+$HHTcount+$HTHcount+$HTTcount+$THHcount+$THTcount+$TTHcount+$TTTcount))*100}"`
+echo "TTT combination percentage is:" `awk "BEGIN {print $TTTcount/(($HHHcount+$HHTcount+$HTHcount+$HTTcount+$THHcount+$THTcount+$TTHcount+$TTTcount))*100}"`

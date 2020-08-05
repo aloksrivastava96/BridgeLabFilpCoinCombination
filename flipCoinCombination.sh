d806267b895aada59fedc7229bffb5467cd663e5
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 16:36:50 +0530

    Added Doublet Combinations and percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -24,3 +24,37 @@ echo ${SingletDict[@]}
 echo "H combination percentage is:" `awk "BEGIN {print $Hcount/(($Hcount+$Tcount))*100}"`
 echo "T combination percentage is:" `awk "BEGIN {print $Tcount/(($Hcount+$Tcount))*100}"`
 
+counter=10
+declare -A DoubletDict
+HHcount=0
+HTcount=0
+THcount=0
+TTcount=0
+while ((counter>0))
+do
+        toss1=$((RANDOM%2))
+	toss2=$((RANDOM%2))
+        if (( ((toss==1)) && ((toss2==1)) ))
+        then
+                ((HHcount++))
+        elif (( ((toss==1)) && ((toss2==0)) ))
+	then
+                ((HTcount++))
+	elif (( ((toss==0)) && ((toss2==1)) ))
+	then
+		((THcount++))
+	else
+		((TTcount++))
+        fi
+        ((counter--))
+done
+DoubletDict[HH]=$HHcount
+DoubletDict[HT]=$HTcount
+DoubletDict[TH]=$THcount
+DoubletDict[TT]=$TTcount
+echo ${!DoubletDict[@]}
+echo ${DoubletDict[@]}
+echo "HH combination percentage is:" `awk "BEGIN {print $HHcount/(($HHcount+$HTcount+$THcount+$TTcount))*100}"`
+echo "HT combination percentage is:" `awk "BEGIN {print $HTcount/(($HHcount+$HTcount+$THcount+$TTcount))*100}"`
+echo "TH combination percentage is:" `awk "BEGIN {print $THcount/(($HHcount+$HTcount+$THcount+$TTcount))*100}"`
+echo "TT combination percentage is:" `awk "BEGIN {print $TTcount/(($HHcount+$HTcount+$THcount+$TTcount))*100}"`

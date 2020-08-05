5aa61d8f42694353a172565008d8650d0d6457f7
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 15:47:12 +0530

    Flipping a coin

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
 echo "This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet"
+
+toss=$((RANDOM%2))
+if ((toss==1))
+then
+        echo "Head"
+else
+        echo "Tail"
+fi
